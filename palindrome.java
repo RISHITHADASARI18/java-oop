2e884fe37a350701d91088991169fdd525bd1362
RISHITHADASARI18 <rishithadasari718@gmail.com>
2026-07-13 23:39:57 +0530

    Add palindrome program

---
+++ palindrome.java
@@ -0,0 +1,17 @@
+public class palindrome {
+    public static void main(String[] args) {
+        int num=323,rem=0,rev=0,temp;
+        temp=num;
+        while(num>0)
+        {
+            rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        if(temp==rev)
+             System.out.println("palindrome");
+        else
+            System.out.println("not a palindrome");
+    }
+    
+}

efae3ac76fcbe64655102706fc3983d7de9f005b
RISHITHADASARI18 <rishithadasari718@gmail.com>
2026-07-19 21:39:41 +0530

    Added code

---
+++ evenorodd.java
@@ -0,0 +1,14 @@
+
+    public class evenorodd {
+    public static void main(String[] args) {
+    
+        int numberToCheck = 47;
+        if (numberToCheck % 2 == 0) {
+            System.out.println(numberToCheck + " is an EVEN number.");
+        } else {
+            System.out.println(numberToCheck + " is an ODD number.");
+        }
+    }
+}
+    
+
